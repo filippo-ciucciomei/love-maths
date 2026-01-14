d8bedf94f29531887a485c6b98ceb61e411d66f4
filippo-ciucciomei <filippociucciomei@gmail.com>
2026-01-14 14:41:01 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,20 @@
 // Get the button elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
+
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked submit!");
             } else {
                     let gameType = this.getAttribute("data-type");
-                    alert(`you clicked ${gameType}!`); 
+                    runGame(gameType); 
                 }
             
         })
     }
+
+    runGame("addition")
 })
 
 
@@ -20,10 +23,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     // Generate two random numbers between 1 and 25
 let num1 = Math.floor(Math.random() * 25) + 1;
 let num2 = Math.floor(Math.random() * 25) + 1;
+
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+} else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting!`;
+}
+
 }
 
 function checkAnswer() {
@@ -42,7 +53,10 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";  
 
 }
 
